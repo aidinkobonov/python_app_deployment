e969aacb0248aea28db06259d7d71dddc52ee0e9
aidinkobonov <aidinkobonov@gmail.com>
2019-05-04 14:38:32 -0500

    fixed

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,5 @@
 node{
+    properties([parameters([string(defaultValue: 'IP', description: 'Where to build e.g IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
         git "git@github.com:aidinkobonov/flask-examples.git"
     }
